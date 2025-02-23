3c5aa275c0c6fe9415e779cb851a2af5ea38d9d4
zjh <zhou_jin_huai@163.com>
2025-02-23 15:46:31 +0800

    20250223添加用户管理页面

---
+++ app/[locale]/admin/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from 'react';
+import UserList from "@/components/chat/admin/userList";
+
+export default function App() {
+  return (
+    <div className="App">
+      <UserList />
+    </div>
+  );
+}
